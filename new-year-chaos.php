671cd61a3220d989a87a17ba23205be2d2263754
Samuel Zanferdini Oliva <2183164+samueloliva@users.noreply.github.com>
2020-04-24 20:24:21 -0300

    Update new-year-chaos.php

--- new-year-chaos.php
+++ new-year-chaos.php
@@ -39,20 +39,13 @@ function minimumBribes($q) {
 }
 
 $stdin = fopen("php://stdin", "r");
-
 fscanf($stdin, "%d\n", $t);
-
 for ($t_itr = 0; $t_itr < $t; $t_itr++) {
     fscanf($stdin, "%d\n", $n);
-
     fscanf($stdin, "%[^\n]", $q_temp);
-
     $q = array_map('intval', preg_split('/ /', $q_temp, -1, PREG_SPLIT_NO_EMPTY));
-
     minimumBribes($q);
 }
-
 fclose($stdin);
-
 
 ?>
